c02d5885497d3b7e38741ef668e555211f4b2100
Kevin Vu <kevinvu59@gmail.com>
2021-02-24 15:06:58 -0500

    Switch from storyboard to programmatic UI. Set homeVC's title to 'Dailies' and add setup functions to homeVC

---
+++ DailiesIRL/HomeViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  DailiesIRL
+//
+//  Created by Kevin Vu on 2/24/21.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    override func loadView() {
+        super.loadView()
+        setup()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    // MARK: - Setup
+    
+    private func setupNavigationBar() {
+        title = "Dailies"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    private func setupView() {
+        view.backgroundColor = .systemGroupedBackground
+    }
+    
+    private func setup() {
+        setupNavigationBar()
+        setupView()
+    }
+    
+    
+}
+
