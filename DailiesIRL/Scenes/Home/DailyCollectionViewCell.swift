4c26b4f567d494dd247bbaecf2dc22ef0e97faf5
Kevin Vu <kevinvu59@gmail.com>
2021-03-31 23:50:29 -0400

    Add checkmark image view to cell and refactor subview setup methods

--- DailiesIRL/Scenes/Home/DailyCollectionViewCell.swift
+++ DailiesIRL/Scenes/Home/DailyCollectionViewCell.swift
@@ -22,31 +22,75 @@ final class DailyCollectionViewCell: UICollectionViewCell {
         return label
     }()
     
+    private let checkmarkImageView: UIImageView = {
+        let view = UIImageView(frame: .zero)
+        view.image = UIImage(systemName: "checkmark.circle.fill")
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.tintColor = UIColor(hex: "26CC68")
+        
+        return view
+    }()
+    
     
     // MARK: - Object lifecycle
     override init(frame: CGRect) {
         super.init(frame: frame)
         addSubviews()
-        setupSubviews()
+        configureSubviews()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+}
+
+
+
+// MARK: - Setup
+extension DailyCollectionViewCell {
     
-    // MARK: - View methods
-    override func layoutSubviews() {
-        super.layoutSubviews()
-    }
-    
-    
-    // MARK: - Setup
     private func addSubviews() {
         contentView.addSubview(titleLabel)
+        contentView.addSubview(checkmarkImageView)
     }
     
-    private func setupSubviews() {
+    private func configureLabelConstraints() {
+        // good to set priority of 2 constraints to 999 (i.e. not max):
+        // one vertical and one horizontal - probably best to do bottom and trailing
+        let labelTopAnchorConstraint = titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 20)
+        let labelBottomAnchorConstraint = titleLabel.bottomAnchor.constraint(lessThanOrEqualTo: contentView.bottomAnchor)
+        labelBottomAnchorConstraint.priority = UILayoutPriority(999)
+        let labelLeadingAnchorConstraint = titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 64)
+        let labelTrailingAnchorConstraint = titleLabel.trailingAnchor.constraint(lessThanOrEqualTo: contentView.trailingAnchor, constant: -15)
+        labelTrailingAnchorConstraint.priority = UILayoutPriority(999)
+        
+        NSLayoutConstraint.activate([
+            labelTopAnchorConstraint,
+            labelBottomAnchorConstraint,
+            labelLeadingAnchorConstraint,
+            labelTrailingAnchorConstraint
+        ])
+    }
+    
+    private func configureCheckmarkImageViewConstraints() {
+        let checkmarkTopAnchorConstraint = checkmarkImageView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
+        let checkmarkHeightAnchorConstraint = checkmarkImageView.heightAnchor.constraint(equalToConstant: 25)
+        checkmarkHeightAnchorConstraint.priority = UILayoutPriority(999)
+        
+        let checkmarkLeadingAnchorConstraint = checkmarkImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 21)
+        let checkmarkWidthAnchorConstraint = checkmarkImageView.widthAnchor.constraint(equalToConstant: 25)
+        checkmarkWidthAnchorConstraint.priority = UILayoutPriority(999)
+        
+        NSLayoutConstraint.activate([
+            checkmarkTopAnchorConstraint,
+            checkmarkHeightAnchorConstraint,
+            checkmarkLeadingAnchorConstraint,
+            checkmarkWidthAnchorConstraint
+        ])
+    }
+    
+    private func configureSubviews() {
         // setting view of cell here
         backgroundColor = .white
         layer.cornerRadius = 25
@@ -56,14 +100,7 @@ final class DailyCollectionViewCell: UICollectionViewCell {
             radius: 6)
         layer.masksToBounds = false
         
-        contentView.translatesAutoresizingMaskIntoConstraints = false
-        
-        // constraints setup here
-        NSLayoutConstraint.activate([
-            titleLabel.topAnchor.constraint(equalTo: contentView.readableContentGuide.topAnchor),
-            titleLabel.bottomAnchor.constraint(lessThanOrEqualTo: contentView.bottomAnchor),
-            titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 64),
-            titleLabel.trailingAnchor.constraint(lessThanOrEqualTo: contentView.trailingAnchor)
-        ])
+        configureLabelConstraints()
+        configureCheckmarkImageViewConstraints()
     }
 }
